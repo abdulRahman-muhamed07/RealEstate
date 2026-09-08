0a4e85337e95ce2dba30c4565cd8d8f719e22f46
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-08 19:50:47 +0300

    Fix DatabaseInitializer GetMigrationsAsync for EF Core 10

--- src/RealEstate.Infrastructure/Persistence/DatabaseInitializer.cs
+++ src/RealEstate.Infrastructure/Persistence/DatabaseInitializer.cs
@@ -12,7 +12,7 @@ public static class DatabaseInitializer
     {
         await using var scope = services.CreateAsyncScope();
         var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        var migrations = await db.Database.GetMigrationsAsync();
+        var migrations = db.Database.GetMigrations();
 
         if (migrations.Any())
         {
